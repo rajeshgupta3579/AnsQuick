a6674c089cf3c43ae347b9493749325fa111b5ff
Ashutosh Jindal <ashujindal92@gmail.com>
2016-06-28 17:41:05 +0530

    implementing tag suggester

--- application/models/Ansquick_model.php
+++ application/models/Ansquick_model.php
@@ -12,6 +12,38 @@ class Ansquick_model extends CI_Model{
           $query = $this->db->query($sql);
           return $query;
      }
+
+     /*
+     * A function to give suggestions of tags when user is posting a question
+     * Input is taken from the request made by jquery
+     * Output is list of suggestions in json format
+     */
+     function getTags(){
+
+
+              if(!isset($_REQUEST['term']))
+              exit();
+              $sql = 'SELECT * FROM Tags WHERE tagName	LIKE "'.ucfirst($_REQUEST['term']).'%" ORDER BY tagID ASC	LIMIT 0,10';
+              $query = $this->db->query($sql);
+              $data = array();
+
+              while($query->result() as $row)
+              	$data[] = array(
+              		'label' => $row['tagName'],
+              		'value' => $row['tagName'],
+              	);
+              }
+
+              echo json_encode($data);
+              flush();
+
+     }
+
+
+
+
+
+
      /*
      * Encrypts the user password.
      * Takes the user entered password as input and adds random salt to it and perform encryption.
@@ -36,7 +68,7 @@ class Ansquick_model extends CI_Model{
           if($query->num_rows()>0){
             $row = $query->result();
             $encryptedPassword = $this->encryptPassword($password,$row[0]->salt);
-//            print_r($row);            
+//            print_r($row);
             if($encryptedPassword==$row[0]->password){
               return 1;
             }
--- application/views/AnsQuick/footer.php
+++ application/views/AnsQuick/footer.php
@@ -1,5 +1,12 @@
 </body>
+
+				<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+				<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+				<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url('js/index.js'); ?>"></script>
+
 </html>
