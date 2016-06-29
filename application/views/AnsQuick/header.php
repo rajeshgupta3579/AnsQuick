cfacc514429ec49ad5fd0abf26e4ded1ef6208a1
Rajesh Gupta <rajesh.gupta@99acres.com>
2016-06-29 15:55:29 +0530

    changes

--- application/views/AnsQuick/header.php
+++ application/views/AnsQuick/header.php
@@ -64,57 +64,61 @@
         </div>
         </form>
     </div>
-    <ul class="nav navbar-nav navbar-right">
+    <ul class="nav navbar-nav navbar-right" style="margin-right:50px">
       <li><a href="#postQuestion" data-toggle="modal">Post Question!!</a></li>
       <li><a href="<?php echo base_url('index.php/AnsQuick/logout'); ?>">Logout</a></li>
-      <?php      
+      <?php
       if(!$this->session->userdata('userID')) {
 
         ?>
         <li><a href="#logIn" data-toggle="modal">Log In/Sign Up</a></li> </ul>
       <?php }  else{?>
+        <li>
+          <ul class="nav navbar-nav navbar-right">
+                      <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                              <span class="glyphicon glyphicon-user"></span> 
+                              <strong>Nombre</strong>
+                              <span class="glyphicon glyphicon-chevron-down"></span>
+                          </a>
+                          <ul class="dropdown-menu">
+                              <li>
+                                  <div class="navbar-login">
+                                      <div class="row">
+                                          <div class="col-lg-4">
+                                              <p class="text-center">
+                                                  <span class="glyphicon glyphicon-user icon-size"></span>
+                                              </p>
+                                          </div>
+                                          <div class="col-lg-8">
+                                              <p class="text-left"><strong>Nombre Apellido</strong></p>
+                                              <p class="text-left small">correoElectronico@email.com</p>
+                                              <p class="text-left">
+                                                  <a href="#" class="btn btn-primary btn-block btn-sm">Actualizar Datos</a>
+                                              </p>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </li>
+                              <li class="divider"></li>
+                              <li>
+                                  <div class="navbar-login navbar-login-session">
+                                      <div class="row">
+                                          <div class="col-lg-12">
+                                              <p>
+                                                  <a href="#" class="btn btn-danger btn-block">Cerrar Sesion</a>
+                                              </p>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </li>
+                          </ul>
+                      </li>
+                  </ul>
+
+        </li>
         </ul>
-        <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <span class="glyphicon glyphicon-user"></span> 
-                            <strong>Nombre</strong>
-                            <span class="glyphicon glyphicon-chevron-down"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <div class="navbar-login">
-                                    <div class="row">
-                                        <div class="col-lg-4">
-                                            <p class="text-center">
-                                                <span class="glyphicon glyphicon-user icon-size"></span>
-                                            </p>
-                                        </div>
-                                        <div class="col-lg-8">
-                                            <p class="text-left"><strong>Nombre Apellido</strong></p>
-                                            <p class="text-left small">correoElectronico@email.com</p>
-                                            <p class="text-left">
-                                                <a href="#" class="btn btn-primary btn-block btn-sm">Actualizar Datos</a>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <div class="navbar-login navbar-login-session">
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <p>
-                                                <a href="#" class="btn btn-danger btn-block">Cerrar Sesion</a>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+
     <?php }?>
   </div><!-- /.navbar-collapse -->
 </nav>
